32ba21efc19a8246ab53b12b08d1c3c39aa4220b
Asher Maxwell <ashermaxwellprogramming@gmail.com>
2025-11-09 14:57:24 -0500

    Added a new Class

--- GoFish/src/main/java/com/gofish/model/Card.java
+++ GoFish/src/main/java/com/gofish/model/Card.java
@@ -1,5 +1,62 @@
 package main.java.com.gofish.model;
 
 public class Card {
+	//Attributes
+		private String rank;
+		private String suit;
+		private String colour;
+		
+		//Default 
+		public Card() {
+			this.rank = "?";
+			this.suit = "?";
+			this.colour = "?";
+		}
+		
+		//primary
+		public Card(String rank, String suit, String colour) {
+			this.rank = rank;
+			this.suit = suit;
+			this.colour = colour;
+		}
+		
+		//Copy
+		public Card(Card card) {
+			this.rank = card.rank;
+			this.suit = card.suit;
+			this.colour = card.colour;
+		}
 
+		public String getRank() {
+			return this.rank;
+		}
+
+		public void setRank(String rank) {
+			this.rank = rank;
+		}
+
+		public String getSuit() {
+			return this.suit;
+		}
+
+		public void setSuit(String suit) {
+			this.suit = suit;
+		}
+
+		public String getColour() {
+			return this.colour;
+		}
+
+		public void setColour(String colour) {
+			this.colour = colour;
+		}
+		
+		
+		public String toString() {
+			return "Rank: " + rank + 
+					"\nSuit: " + suit +
+					"\nColour: " + colour + "\n";
+		}
+		
+		
 }
